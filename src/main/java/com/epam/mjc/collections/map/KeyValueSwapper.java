201faed1dbc4118c24ffe3ca281c44e6e145ad29
BorisSolomonia <78170135+BorisSolomonia@users.noreply.github.com>
2022-10-06 10:43:51 +0400

    Update KeyValueSwapper.java

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,23 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Integer n;
+        String s;
+        Map<String, Integer> reverse = new HashMap<>();
+        Map<Integer, String> copy = new HashMap<>();
+        for (Integer f : sourceMap.keySet()) {
+            s = sourceMap.get(f);
+            if(reverse.containsKey(s) && reverse.get(s) > f){
+                reverse.put(s,f);
+            }else {
+                reverse.put(s,f);
+            }
+
+        }
+        return reverse;
     }
 }
