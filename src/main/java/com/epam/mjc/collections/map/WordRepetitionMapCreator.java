700944985babb0390dadc922c2c388a99edfa605
BorisSolomonia <78170135+BorisSolomonia@users.noreply.github.com>
2022-10-06 10:44:15 +0400

    Update WordRepetitionMapCreator.java

--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,25 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        String[] splitSentence = sentence.split("[ .,]");
+        Map<String,Integer> wordCount = new HashMap<>();
+        for (String k: splitSentence) {
+            System.out.println(k + " ... "+ wordCount.containsKey(k));
+
+            if (wordCount.containsKey(k.toLowerCase(Locale.ROOT)) && k.length() > 0){
+
+                int n = wordCount.get(k.toLowerCase(Locale.ROOT))+1;
+                System.out.println(n);
+                wordCount.put(k.toLowerCase(Locale.ROOT), n);
+            }else if(k.length() > 0){
+                wordCount.put(k.toLowerCase(Locale.ROOT), 1);
+            }
+        }
+        return wordCount;
     }
 }
